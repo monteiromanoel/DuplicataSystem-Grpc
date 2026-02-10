39ad995be99d0c0f74ef7d8b5881a8a65a1fa6f5
manoel-esx <manoel.monteiro@esx.com.br>
2026-02-10 18:31:24 -0300

    Added GRPC Client to ApiGateway

---
+++ Duplicata.ApiGateway/Controllers/DuplicatasController.cs
@@ -0,0 +1,31 @@
+﻿using Duplicata.GrpcService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Duplicata.ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DuplicatasController : ControllerBase
+    {
+        private readonly DuplicataService.DuplicataServiceClient _grpc;
+
+        public DuplicatasController(DuplicataService.DuplicataServiceClient duplicataServiceClient)
+        {
+            _grpc = duplicataServiceClient;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateDuplicataRequest request)
+        {
+            var response = await _grpc.CreateDuplicataAsync(request);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _grpc.GetDuplicatasAsync(new Empty());
+            return Ok(response);
+        }
+    }
+}
--- Duplicata.ApiGateway/Program.cs
+++ Duplicata.ApiGateway/Program.cs
@@ -1,42 +1,22 @@
+using Duplicata.GrpcService;
+using Grpc.Net.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// gRPC client
+builder.Services.AddGrpcClient<DuplicataService.DuplicataServiceClient>(o =>
+{
+    o.Address = new Uri("https://localhost:7004"); // porta do GrpcService
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
